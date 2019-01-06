fd49b55eca31b7bdc45c91423c553756d2719210
Katrina Agustin <katrina.agustin@gmail.com>
2019-01-05 18:33:11 -0800

    Labels and Disabled States - labels are functional - added disabled states for the toolbar - updated findUnread method - removed some helper methods that aren't needed

--- src/Toolbar.js
+++ src/Toolbar.js
@@ -11,26 +11,26 @@ export default class Toolbar extends Component {
             unread message(s)
           </p>
 
-          <button className="btn btn-default" onClick={() => this.props.selectAll()} >
+          <button className="btn btn-default" onClick={() => this.props.selectAll()} disabled={this.props.stateLength() === 0}>
             <i className={`fa ${this.props.selectIcons()}`} ></i>
           </button>
 
-          <button className="btn btn-default" onClick={() => this.props.handleRead(true)} disabled={this.props.findUnread(false) === 0}>
+          <button className="btn btn-default" onClick={() => this.props.handleRead(true)} disabled={this.props.findUnread(false) === 0 || this.props.stateLength() === 0}>
             Mark As Read
           </button>
 
-          <button className="btn btn-default" onClick={() => this.props.handleRead(false)} disabled={this.props.findUnread(true) === 0}>
+          <button className="btn btn-default" onClick={() => this.props.handleRead(false)} disabled={this.props.findUnread(true) === 0 || this.props.stateLength() === 0}>
             Mark As Unread
           </button>
 
-          <select className="form-control label-select" defaultValue="Apply Label" onChange={(e) => this.props.handleLabels(e.target.value, 'addLabel')}>
+          <select className="form-control label-select" defaultValue="Apply Label" onChange={(e) => this.props.handleLabels(e.target.value, 'addLabel')} disabled={this.props.stateLength() === 0}>
             <option value="Apply Label" disabled>Apply label</option>
             <option value="dev">dev</option>
             <option value="personal">personal</option>
             <option value="gschool">gschool</option>
           </select>
 
-          <select className="form-control label-select" defaultValue="Remove Label" onChange={(e) => this.props.handleLabels(e.target.value, 'removeLabel')}>
+          <select className="form-control label-select" defaultValue="Remove Label" onChange={(e) => this.props.handleLabels(e.target.value, 'removeLabel')} disabled={this.props.stateLength() === 0}>
             <option value="Remove Label" disabled>Remove label</option>
             <option value="dev">dev</option>
             <option value="personal">personal</option>
